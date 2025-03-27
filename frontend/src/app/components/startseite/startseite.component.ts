364f3c6713cf1f65b6b05edf96726d60c5fa835d
lisaschifrer <lisa.schifrer@gmail.com>
2025-03-27 16:53:12 +0100

    feat: add logo

---
+++ frontend/src/app/components/startseite/startseite.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-startseite',
+  imports: [],
+  templateUrl: './startseite.component.html',
+  styleUrl: './startseite.component.scss'
+})
+export class StartseiteComponent {
+
+}
