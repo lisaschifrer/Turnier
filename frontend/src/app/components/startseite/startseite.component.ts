0320d94ccfebc1ee84ddf1ad31d1fecd04876114
lschifrer <lschifrer@imendo.at>
2025-03-28 12:01:44 +0100

    feat: fiinish startseite

--- frontend/src/app/components/startseite/startseite.component.ts
+++ frontend/src/app/components/startseite/startseite.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatError } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-startseite',
-  imports: [],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatError,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './startseite.component.html',
   styleUrl: './startseite.component.scss'
 })
 export class StartseiteComponent {
+  turniername: string = '';
 
+  onTurnierStarten() {
+    console.log('Turniername: ', this.turniername);
+  }
 }
