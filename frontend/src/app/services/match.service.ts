fa30b9f37e6e5981754795f757e41cbf03c4a1b6
lisaschifrer <lisa.schifrer@gmail.com>
2025-08-25 15:07:22 +0200

    feat: add MatchService

---
+++ frontend/src/app/services/match.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { environment } from "../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({providedIn: 'root'})
+export class MatchService{
+    private apiUrl = `${environment.apiUrl}/GroupMatches`;
+
+    constructor(private http: HttpClient) {}
+
+    generateMatches(turnierId: string): Observable<Match[]> {
+        return this.http.post<Match[]>(`${this.apiUrl}/generate/${turnierId}`, {})
+    }
+
+    setWinner(matchId: string, winnerId:string): Observable<Match[]> {
+        return this.http.post<Match[]>(`${this.apiUrl}/${matchId}/winner/${winnerId}`, {});
+    }
+}
+
+export interface Match {
+    id: string;
+    groupId: string;
+    teamA: string;
+    teamB: string;
+    winnerId?: string;
+}
